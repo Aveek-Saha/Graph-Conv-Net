40e7882d8386ae8e3ff819f531ced6c18d48b545
Aveek Saha <aveek.s98@gmail.com>
2020-08-23 19:15:01 +0530

    Separate normalizaion

--- gcn.py
+++ gcn.py
@@ -3,6 +3,18 @@ import numpy as np
 import networkx as nx
 import matplotlib.pyplot as plt
 
+
+def norm_adjacency_matrix(A):
+  I = tf.eye(tf.shape(A)[0])
+  A_hat = A + I
+
+  D_inv = tf.linalg.tensor_diag(
+      tf.pow(tf.reduce_sum(A, 0), tf.cast(-0.5, tf.float32)))
+
+  A_hat = D_inv @ A_hat @ D_inv
+
+  return A_hat
+
 class GraphConvolutionLayer(tf.keras.layers.Layer):
     def __init__(self, A, input_units, output_units):
         super(GraphConvolutionLayer, self).__init__()
