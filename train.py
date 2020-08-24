43fa6eb485034d66476398914a8c8bb1bb0eace8
Aveek Saha <aveek.s98@gmail.com>
2020-08-24 12:47:10 +0530

    Crate model and fit

--- train.py
+++ train.py
@@ -3,6 +3,8 @@ import numpy as np
 import os
 import csv
 
+from gcn import *
+
 from sklearn.utils import shuffle
 from sklearn.preprocessing import LabelEncoder
 
@@ -67,4 +69,43 @@ def encode_label(labels):
     return labels, label_encoder.classes_
 
 
+G = nx.Graph()
+G.add_nodes_from(nodes)
+G.add_edges_from(edge_list)
+
+A = nx.convert_matrix.to_numpy_matrix(G)
+A = tf.convert_to_tensor(A, tf.float32)
+print('Graph info: ', nx.info(G))
+
+A = norm_adjacency_matrix(A)
+
+l2 = 5e-4
+rate = 0.5
+epochs = 200
+learning_rate = 1e-2 
 labels_encoded, classes = encode_label(labels)
+
+inp = tf.keras.Input((features.shape[1],))
+out_1 = GraphConvolutionLayer(A, 16, rate, l2)(inp)
+out_2 = GraphConvolutionLayer(A, 7, rate, l2)(out_1)
+out = tf.keras.layers.Dense(7, activation="softmax")(out_2)
+
+model = tf.keras.Model(inputs=inp, outputs=out, name="graph_convolution")
+
+optimizer = tf.keras.optimizers.Adam(learning_rate=learning_rate)
+
+model.compile(optimizer=optimizer,
+              loss='categorical_crossentropy',
+              weighted_metrics=['acc'])
+
+print(model.summary())
+
+validation_data = (features, labels_encoded, val_mask)
+model.fit(features,
+          labels_encoded,
+          sample_weight=train_mask,
+          epochs=epochs,
+          batch_size=num_nodes,
+          validation_data=validation_data,
+          shuffle=False)
+
